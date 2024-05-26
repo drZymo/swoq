30042471ca2826ba1fb1c6e9b4ce236d8355e35b
Ralph Schiedon <drZymo@users.noreply.github.com>
2024-05-26 11:55:03 +0200

    Create own GroupBox control

---
+++ InfraUI/Views/GroupBox.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Swoq.InfraUI.Views;
+
+public partial class GroupBox : UserControl
+{
+    public static readonly StyledProperty<string> HeaderProperty =
+        AvaloniaProperty.Register<GroupBox, string>(nameof(Header), defaultValue: "");
+
+    public string Header
+    {
+        get { return GetValue(HeaderProperty); }
+        set { SetValue(HeaderProperty, value); }
+    }
+}
