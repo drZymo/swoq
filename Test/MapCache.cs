ce33c4885be00d0e91b502a732d53e46e5f60c72
Ralph Schiedon <drZymo@users.noreply.github.com>
2024-06-28 07:30:13 +0200

    Add map cache for simpler tests.
    Tests for map cache added first, before using it in tests.

---
+++ Test/MapCache.cs
@@ -0,0 +1,60 @@
+﻿using Swoq.Server;
+using System.Collections.Immutable;
+using Position = System.ValueTuple<int, int>;
+
+namespace Swoq.Test;
+
+internal class MapCache(int height, int width, int visibilityRange)
+{
+    private int[] cache = new int[height * width];
+
+    public int this[int y, int x]
+    {
+        get
+        {
+            if (y < 0 || y >= height) throw new ArgumentOutOfRangeException(nameof(y));
+            if (x < 0 || x >= width) throw new ArgumentOutOfRangeException(nameof(x));
+            return cache[y * width + x];
+        }
+    }
+
+    public IImmutableDictionary<Position, (int from, int to)> AddPlayerStates(
+        PlayerState? playerState1, PlayerState? playerState2)
+    {
+        var changes = ImmutableDictionary<Position, (int from, int to)>.Empty;
+
+        AddOnePlayerState(playerState1, ref changes);
+        AddOnePlayerState(playerState2, ref changes);
+
+        return changes;
+    }
+
+    private void AddOnePlayerState(PlayerState? playerState, ref ImmutableDictionary<(int, int), (int from, int to)> changes)
+    {
+        if (playerState == null) return;
+
+        var top = playerState.Position.y - visibilityRange;
+        var left = playerState.Position.x - visibilityRange;
+        var surroundingsSize = visibilityRange * 2 + 1;
+
+        for (int sy = 0; sy < surroundingsSize; sy++)
+        {
+            var my = top + sy;
+            for (int sx = 0; sx < surroundingsSize; sx++)
+            {
+                var mx = left + sx;
+
+                if (0 <= my && my < height && 0 <= mx && mx < width)
+                {
+                    var newValue = playerState.Surroundings[sy * surroundingsSize + sx];
+                    var cacheIndex = my * width + mx;
+                    if (cache[cacheIndex] != newValue)
+                    {
+                        changes = changes.SetItem((my, mx), (cache[cacheIndex], newValue));
+                        cache[cacheIndex] = newValue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Test/MapCacheTests.cs
@@ -0,0 +1,73 @@
+﻿using Swoq.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swoq.Test;
+
+[TestFixture]
+public class MapCacheTests
+{
+    [Test]
+    public void SameStateNoChanges()
+    {
+        MapCache cache = new(8, 8, 2);
+
+        var playerState = new PlayerState(
+            (1, 1), 5, 0, false, new int[] { 
+                0, 0, 0, 0, 0,
+                0, 3, 3, 3, 3,
+                0, 3, 2, 1, 1,
+                0, 3, 1, 1, 1,
+                0, 3, 1, 1, 1,
+            });
+
+        cache.AddPlayerStates(playerState, null);
+
+        var changes = cache.AddPlayerStates(playerState, null);
+
+        Assert.That(changes, Is.Empty);
+    }
+
+    [Test]
+    public void MoveEastAddsColumn()
+    {
+        MapCache cache = new(8, 8, 2);
+
+        var playerState1 = new PlayerState(
+            (1, 1), 5, 0, false, new int[] {
+                0, 0, 0, 0, 0,
+                0, 3, 3, 3, 3,
+                0, 3, 2, 1, 1,
+                0, 3, 1, 1, 1,
+                0, 3, 1, 1, 1,
+            });
+
+        cache.AddPlayerStates(playerState1, null);
+
+        var playerState2 = new PlayerState(
+            (1, 2), 5, 0, false, new int[] {
+                0, 0, 0, 0, 0,
+                3, 3, 3, 3, 3,
+                3, 1, 2, 1, 1,
+                3, 1, 1, 1, 1,
+                3, 1, 1, 1, 1,
+            });
+
+        var changes = cache.AddPlayerStates(playerState2, null);
+        Assert.That(changes, Is.Not.Empty);
+
+        // column of 4 and 2 player positions (old and new)
+        Assert.That(changes.Count, Is.EqualTo(6));
+
+        Assert.That(changes[(0, 4)], Is.EqualTo((0, 3)));
+        Assert.That(changes[(1, 4)], Is.EqualTo((0, 1)));
+        Assert.That(changes[(2, 4)], Is.EqualTo((0, 1)));
+        Assert.That(changes[(3, 4)], Is.EqualTo((0, 1)));
+        
+        Assert.That(changes[(1, 1)], Is.EqualTo((2, 1)));
+        Assert.That(changes[(1, 2)], Is.EqualTo((1, 2)));
+    }
+}
