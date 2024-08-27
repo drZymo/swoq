a83ce43a8f4ba55e0a7b10814eed688f33d35970
Ralph Schiedon <drZymo@users.noreply.github.com>
2024-08-27 22:20:48 +0200

    Add example C# bot

---
+++ Bot/ExampleCSharp/GamePlayerException.cs
@@ -0,0 +1,8 @@
+﻿class GamePlayerException : Exception
+{
+    public Swoq.Interface.Result Result { get; init; }
+}
+
+
+
+
---
+++ Bot/ExampleCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Swoq.Interface;
+
+var userId = "<insert your user id here>";
+int? level = 0; // null for quest, integer for train
+
+using var player = new GamePlayer(userId);
+
+var state = player.Start(level);
+var moveEast = true;
+while (!state.Finished)
+{
+    state = player.Act(moveEast ? DirectedAction.MoveEast : DirectedAction.MoveSouth);
+    moveEast = !moveEast;
+}
