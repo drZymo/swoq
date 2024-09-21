63985bfc47ca807b467452a59f45b83929e0c7c9
Ralph Schiedon <drZymo@users.noreply.github.com>
2024-09-21 16:25:45 +0200

    Tool to quickly run map generator in parallel

---
+++ MapGeneratorDurationTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using Swoq.Infra;
+
+IEnumerable<int> RandomLevels()
+{
+    var random = new Random();
+    while (true)
+    {
+        yield return random.Next(MapGenerator.MaximumLevel + 1);
+    }
+}
+
+Parallel.ForEach(RandomLevels(), level =>
+{
+    MapGenerator.Generate(level, 64, 64);
+});
