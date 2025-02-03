65ee4f0df11d46c4933758361833b8c0e7f5cf33
Martin Poelstra <martin@beryllium.net>
2025-02-03 22:07:00 +0100

    Linux usability (#14)
    * Ensure solution file keeps its line endings in Linux.
    
    * Trim whitespace + final newline for consistency.
    
    * Add Bot to main sln to ensure it keeps building and ease of use on VSCode.
    
    * Fix Example C# bot since recent PR.
    
    * Commit Bot packages.lock.json.
    
    * Move Replays to folder relative to repo root.
    
    * Add prereqs + running to README.
    
    * Fix crash in ReplayViewer.

--- Bot/ExampleCSharp/Program.cs
+++ Bot/ExampleCSharp/Program.cs
@@ -7,7 +7,7 @@ using var player = new GamePlayer(userId);
 
 var state = player.Start(level);
 var moveEast = true;
-while (!state.Finished)
+while (state.Status == GameStatus.Active)
 {
     state = player.Act(moveEast ? DirectedAction.MoveEast : DirectedAction.MoveSouth);
     moveEast = !moveEast;
