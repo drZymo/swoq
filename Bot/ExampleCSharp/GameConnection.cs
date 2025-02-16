6e9139f08411d17d1ae85c28ba3875bbeb2360dd
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-02-16 16:29:12 +0100

    Update bot to be more similar to TypeScript bot (#22)
    * Update bot to be more similar to TypeScript bot
    
    * Only copy if it exists

---
+++ Bot/ExampleCSharp/GameConnection.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Net.Client;
+using Swoq.Interface;
+
+namespace Bot;
+
+internal class GameConnection : IDisposable
+{
+    private readonly GrpcChannel channel;
+    private readonly GameService.GameServiceClient client;
+
+    public GameConnection()
+    {
+        channel = GrpcChannel.ForAddress($"http://{Env.Host}:5080");
+        client = new GameService.GameServiceClient(channel);
+    }
+
+    public void Dispose()
+    {
+        channel.Dispose();
+    }
+
+    public Game Start(int? level)
+    {
+        var request = new StartRequest() { UserId = Env.UserId };
+        if (level.HasValue) request.Level = level.Value;
+
+        StartResponse? response = null;
+        while (true)
+        {
+            response = client.Start(request);
+
+            if (response.Result == Result.Ok) break;
+
+            if (response.Result == Result.QuestQueued)
+            {
+                Console.WriteLine("Quest queued, waiting 2 seconds before retrying ...");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                continue;
+            }
+
+            if (response.Result != Result.Ok)
+            {
+                throw new GameException($"Start failed (result {response.Result})");
+            }
+        }
+
+        return new Game(client, response);
+    }
+}
--- Bot/ExampleCSharp/Program.cs
+++ Bot/ExampleCSharp/Program.cs
@@ -1,14 +1,17 @@
-﻿using Swoq.Interface;
+﻿using Bot;
+using Swoq.Interface;
 
-var userId = "<insert your user id here>";
-int? level = 0; // null for quest, integer for train
+using var connection = new GameConnection();
 
-using var player = new GamePlayer(userId);
+var game = connection.Start(null); // null for quest, integer for train
+Console.WriteLine($"game id: {game.GameId}");
+Console.WriteLine($"map size: {game.MapHeight}x{game.MapWidth}");
 
-var state = player.Start(level);
 var moveEast = true;
-while (state.Status == GameStatus.Active)
+while (game.State.Status == GameStatus.Active)
 {
-    state = player.Act(moveEast ? DirectedAction.MoveEast : DirectedAction.MoveSouth);
+    var action = moveEast ? DirectedAction.MoveEast : DirectedAction.MoveSouth;
+    Console.WriteLine($"tick: {game.State.Tick}, action: {action}");
+    game.Act(action);
     moveEast = !moveEast;
 }
