9e24cf4e49c1f1936758db65be39bfa8aabb84dc
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-02-23 22:21:02 +0100

    Use ProtectedLocalStorage instead of JSInterop
    This works from the OnInitialized method, making the code cleaner.
    Also moved the retrieval of the user to the UserService.

--- Portal/Services/UserService.cs
+++ Portal/Services/UserService.cs
@@ -1,27 +1,31 @@
-using Microsoft.JSInterop;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Swoq.Data;
 
-public class UserService
+public class UserService(ISwoqDatabase database, ProtectedLocalStorage localStorage)
 {
-    private readonly IJSRuntime _jsRuntime;
-
-    public UserService(IJSRuntime jsRuntime)
-    {
-        _jsRuntime = jsRuntime;
-    }
-
     public async Task SetUserIdAsync(string userId)
     {
-        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "userId", userId);
+        await localStorage.SetAsync("userId", userId);
     }
 
-    public async Task<string> GetUserIdAsync()
+    public async Task<User?> GetCurrentUserAsync()
     {
-        return await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "userId");
+        try
+        {
+            var userId = await localStorage.GetAsync<string>("userId");
+            if (!userId.Success || userId.Value == null) return null;
+
+            var user = await database.FindUserByIdAsync(userId.Value);
+            return user;
+        }
+        catch
+        {
+            return null;
+        }
     }
 
     public async Task RemoveUserIdAsync()
     {
-        await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "userId");
+        await localStorage.DeleteAsync("userId");
     }
 }
