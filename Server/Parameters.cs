aa93d2c24b482fd9372c15eae6013881b9f43c66
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-03-06 09:02:58 +0100

    Change default map height to 48

--- Server/Parameters.cs
+++ Server/Parameters.cs
@@ -2,7 +2,7 @@
 
 internal static class Parameters
 {
-    public const int MapHeight = 64;
+    public const int MapHeight = 48;
     public const int MapWidth = 64;
     public const int PlayerVisibilityRange = 8;
     public const int EnemyVisibilityRange = 5;
