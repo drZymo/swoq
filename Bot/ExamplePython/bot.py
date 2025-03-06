0ff5850835ec1721f8b0dbaa603c1d153d714712
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-03-06 08:22:24 +0100

    Keep replay file open and add date/time to Python bot

--- Bot/ExamplePython/bot.py
+++ Bot/ExamplePython/bot.py
@@ -1,20 +1,20 @@
 from swoq import GameConnection
 import swoq_pb2
 
-level = None # None for quest, 0.. (i.e. integer) for train
+level = 0 # None for quest, 0.. (i.e. integer) for train
 
 def main() -> None:
     with GameConnection() as connection:
-        game = connection.start(level)
-        print(f'game id: {game.game_id}')
-        print(f'map size: {game.map_height}x{game.map_width}')
+        with connection.start(level) as game:
+            print(f'game id: {game.game_id}')
+            print(f'map size: {game.map_height}x{game.map_width}')
 
-        move_east = True
-        while game.state.status == swoq_pb2.GAME_STATUS_ACTIVE:
-            action = swoq_pb2.DIRECTED_ACTION_MOVE_EAST if move_east else swoq_pb2.DIRECTED_ACTION_MOVE_SOUTH
-            print(f'tick: {game.state.tick}, action: {action}')
-            game.act(action)
-            move_east = not move_east
+            move_east = True
+            while game.state.status == swoq_pb2.GAME_STATUS_ACTIVE:
+                action = swoq_pb2.DIRECTED_ACTION_MOVE_EAST if move_east else swoq_pb2.DIRECTED_ACTION_MOVE_SOUTH
+                print(f'tick: {game.state.tick}, action: {action}')
+                game.act(action)
+                move_east = not move_east
 
 if __name__ == '__main__':
     main()
--- Bot/ExamplePython/swoq.py
+++ Bot/ExamplePython/swoq.py
@@ -12,29 +12,39 @@ env = json.loads(Path('env.json').read_text())
 class ReplayFile:
     def __init__(self, user_name:str, start_request:swoq_pb2.StartRequest, start_response:swoq_pb2.StartResponse):
         sanitized_user_name = urllib.parse.quote(user_name)
-        self.filename = Path() / 'Replays' / f'{sanitized_user_name} - {start_response.gameId}.swoq'
-        
-        if not self.filename.parent.exists():
-            self.filename.parent.mkdir(parents=True)
-            
-        with open(self.filename, 'wb') as file:
-            header = swoq_pb2.ReplayHeader(userName = user_name, dateTime = datetime.now().isoformat())
-            self._write_delimited(file, header)
+        dateTimeStr = datetime.now().strftime('%Y%m%d-%H%M%S')
+        filename = Path() / 'Replays' / f'{sanitized_user_name} - {dateTimeStr} - {start_response.gameId}.swoq'
 
-            self._write_delimited(file, start_request)
-            self._write_delimited(file, start_response)
+        if not filename.parent.exists():
+            filename.parent.mkdir(parents=True)
+
+        self.file = open(filename, 'wb')
+
+        header = swoq_pb2.ReplayHeader(userName = user_name, dateTime = datetime.now().isoformat())
+        self._write_delimited(header)
+
+        self._write_delimited(start_request)
+        self._write_delimited(start_response)
+
+    def close(self) -> None:
+        self.file.close()
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
 
     def append(self, act_request:swoq_pb2.ActionRequest, act_response:swoq_pb2.ActionResponse) -> None:
-        with open(self.filename, 'ab') as file:
-            self._write_delimited(file, act_request)
-            self._write_delimited(file, act_response)
+        self._write_delimited(act_request)
+        self._write_delimited(act_response)
 
-    def _write_delimited(self, file, message) -> None:
+    def _write_delimited(self, message) -> None:
         bytes = message.SerializeToString()
-        self._write_varint(file, len(bytes))
-        file.write(bytes)
-        
-    def _write_varint(self, file, value:int) -> None:
+        self._write_varint(len(bytes))
+        self.file.write(bytes)
+
+    def _write_varint(self, value:int) -> None:
         rem = value
         bytes = bytearray()
         while rem > 0:
@@ -45,8 +55,8 @@ class ReplayFile:
                 b = rem
                 rem -= rem
             bytes.append(b)
-        file.write(bytes)
-        
+        self.file.write(bytes)
+
 
 class GameException(BaseException):
     pass
@@ -56,23 +66,33 @@ class Game:
     def __init__(self, game_service:swoq_pb2_grpc.GameServiceStub, response:swoq_pb2.StartResponse, replay_file:ReplayFile):
         self.game_service = game_service
         self.replay_file = replay_file
-        
+
         self.game_id = response.gameId
         self.map_height = response.height
         self.map_width = response.width
         self.visibility_range = response.visibilityRange
         self.state = response.state
-    
+
+    def close(self) -> None:
+        self.replay_file.close()
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
+
     def act(self, action:swoq_pb2.DirectedAction) -> None:
         request = swoq_pb2.ActionRequest(gameId=self.game_id, action=action)
         response = self.game_service.Act(request)
-        
+
         self.replay_file.append(request, response)
 
         if response.result != swoq_pb2.ACT_RESULT_OK:
             raise GameException(f'Act failed (result {response.result})')
 
         self.state = response.state
+
 
 class GameConnection:
     def __init__(self):
@@ -97,7 +117,7 @@ class GameConnection:
 
             if response.result == swoq_pb2.START_RESULT_OK:
                 break
-            
+
             if response.result == swoq_pb2.START_RESULT_QUEST_QUEUED:
                 print('Quest queued, waiting 2 seconds before retrying ...')
                 sleep(2)
