1d88ff1cab77b6565ccdc307ccc02110a304c78f
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-03-10 21:44:55 +0100

    Move player died to level 0 in proto files (#39)
    * Move player died to level 0 in proto files
    
    Also updated documentation to hint that boulders should be dropped.
    
    * Create a tool to strip the proto file from everything except level 0

---
+++ update_protos.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+pushd %~dp0ProtoStripper
+
+dotnet restore
+dotnet build --no-restore -c Release
+
+"bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleCSharp\swoq.proto"
+"bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExamplePython\swoq.proto"
+"bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleTypeScript\protos\swoq.proto"
+copy /Y "%~dp0Interface\swoq.proto" "%~dp0\Bot\Fast\swoq.proto"
+
+popd
