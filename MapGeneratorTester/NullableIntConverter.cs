d7840ee135d4a4709b01e406606cfbf05daf9826
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-06-08 21:47:06 +0200

    Add seed to MapGeneratorTester

---
+++ MapGeneratorTester/NullableIntConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace Swoq.MapGeneratorTester;
+
+public class NullableIntConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is int valueAsInt ? valueAsInt.ToString(culture) : string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrWhiteSpace(value?.ToString()))
+        {
+            return null;
+        }
+        if (int.TryParse(value?.ToString(), out int result))
+        {
+            return result;
+        }
+        return null;
+    }
+}
