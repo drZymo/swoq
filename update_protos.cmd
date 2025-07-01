30e9ab23eae0d5028a7b09683f2123eface51a86
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-07-01 22:16:21 +0200

    Revert changes to complete TS bot proto file
    Updated ProtoStripper to allow specifying the level to strip. Complete TS bot is limited to level 20.

--- update_protos.cmd
+++ update_protos.cmd
@@ -10,7 +10,8 @@ dotnet build --no-restore -c Release
 "bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleTypeScript\protos\swoq.proto"
 "bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleRust\proto\swoq.proto"
 "bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleGo\proto\swoq.proto"
-copy /Y "%~dp0Interface\swoq.proto" "%~dp0\Bot\CompleteCSharp\swoq.proto"
-copy /Y "%~dp0Interface\swoq.proto" "%~dp0\Bot\CompleteTypeScript\protos\swoq.proto"
+
+"bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0\Bot\CompleteCSharp\swoq.proto" 23
+"bin\Any CPU\Release\net9.0\ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0\Bot\CompleteTypeScript\protos\swoq.proto" 20
 
 popd
--- update_protos.sh
+++ update_protos.sh
@@ -12,5 +12,6 @@ dotnet build --no-restore -c Release
 "./bin/Any CPU/Release/net9.0/ProtoStripper" "${root}/Interface/swoq.proto" "${root}/Bot/ExampleTypeScript/protos/swoq.proto"
 "./bin/Any CPU/Release/net9.0/ProtoStripper" "${root}/Interface/swoq.proto" "${root}/Bot/ExampleRust/proto/swoq.proto"
 "./bin/Any CPU/Release/net9.0/ProtoStripper" "${root}/Interface/swoq.proto" "${root}/Bot/ExampleGo/proto/swoq.proto"
-cp "${root}/Interface/swoq.proto" "${root}/Bot/CompleteCSharp/swoq.proto"
-cp "${root}/Interface/swoq.proto" "${root}/Bot/CompleteTypeScript/protos/swoq.proto"
+
+"./bin/Any CPU/Release/net9.0/ProtoStripper" "${root}/Interface/swoq.proto" "${root}/Bot/CompleteCSharp/swoq.proto" 23
+"./bin/Any CPU/Release/net9.0/ProtoStripper" "${root}/Interface/swoq.proto" "${root}/Bot/CompleteTypeScript/protos/swoq.proto" 20
