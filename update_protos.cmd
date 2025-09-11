f35aafbb2966d478f49665d007fd52e99859f085
Ralph Schiedon <drZymo@users.noreply.github.com>
2025-09-11 17:12:04 +0200

    Remove example bots from this repository
    They have been moved to the `swoq-bots` repository and updated in the meantime.
    
    The update_protos script still creates a stripped down proto file in the Bot folder, that can be used to update the other repo.

--- update_protos.cmd
+++ update_protos.cmd
@@ -5,12 +5,7 @@ pushd %~dp0ProtoStripper
 dotnet restore
 dotnet build --no-restore -c Release
 
-"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleCSharp\swoq.proto"
-"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExamplePython\swoq.proto"
-"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleTypeScript\protos\swoq.proto"
-"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleRust\proto\swoq.proto"
-"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleGo\proto\swoq.proto"
-"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\ExampleCpp\src\swoq.proto"
+"bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0Bot\swoq.proto"
 
 "bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0\Bot\CompleteCSharp\swoq.proto" 23
 "bin\Any CPU\Release\net9.0\Swoq.ProtoStripper.exe" "%~dp0Interface\swoq.proto" "%~dp0\Bot\CompleteTypeScript\protos\swoq.proto" 20
